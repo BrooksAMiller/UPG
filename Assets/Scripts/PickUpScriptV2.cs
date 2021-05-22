59e477a72245cb1bb11fa00d3d018cdbd503d18f
almostcertain <johndouglasgreene@gmail.com>
2021-05-22 19:55:09 -0400

    - Updated highlight detection - New pick up logic (proximity based)

--- Assets/Scripts/PickUpScriptV2.cs
+++ Assets/Scripts/PickUpScriptV2.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PickUpScriptV2 : MonoBehaviour
 {
-    public Transform location;
+    public Transform location; //Where the center of the object will go if picked up
     public Material highlightColor;
     public GameObject player;
     public GameObject particles;
+    private bool isHighlighted;
 
     private void Start()
     {
@@ -20,6 +21,20 @@ public class PickUpScriptV2 : MonoBehaviour
     private void Update()
     {
         HighlightCheck();
+
+        if (isHighlighted == true)
+        {
+
+            if (Input.GetMouseButton(0))
+            {
+                PickUp();
+                particles.SetActive(false);
+            }
+            
+            
+            
+        }
+        
     }
 
     
@@ -32,6 +47,8 @@ public class PickUpScriptV2 : MonoBehaviour
             //this.GetComponent<ParticleSystem>().Play();
             Debug.Log("Close!");
             particles.SetActive(true);
+            isHighlighted = true;
+
 
         }
         else if (dist > 3 && dist > 6)
@@ -39,7 +56,18 @@ public class PickUpScriptV2 : MonoBehaviour
             //this.GetComponent<ParticleSystem>().Stop();
             Debug.Log("Far");
             particles.SetActive(false);
+            isHighlighted = false;
 
         }
+    }
+
+    private void PickUp()
+    {
+        this.gameObject.transform.position = location.position;
+    }
+
+    private void PickupCheck()
+    {
+
     }
 }
